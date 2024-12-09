a7d6283057b50978e5aa6e2214846a938be51c35
dybowskyy <igordyb@gmail.com>
2024-12-09 17:11:47 +0100

    Downloading the transcript

--- main.py
+++ main.py
@@ -1,4 +1,44 @@
 import customtkinter as ctk
+import youtube_transcript_api
+from urllib.parse import urlparse, parse_qs
+import os
+from dotenv import load_dotenv, dotenv_values
+
+load_dotenv()
+YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
+
+def get_transcript(video_id) -> str:
+    print(video_id)
+    try:
+        transcript = youtube_transcript_api.YouTubeTranscriptApi.get_transcript(video_id)
+        return "\n".join([f"{item['text']}" for item in transcript])
+    except youtube_transcript_api.TranscriptsDisabled:
+        return "Transcripts are disabled for this video."
+    except youtube_transcript_api.TooManyRequests:
+        return "Too many requests"
+    except youtube_transcript_api.CouldNotRetrieveTranscript:
+        return "Could not retrieve transcript"
+
+
+def get_video_id_from_url(url) -> str:
+    print(url)
+    if "v=" in url:
+        return url.split("v=")[1].split("&")[0]
+    elif "youtu.be/" in url:
+        return url.split("youtu.be/")[1]
+    else:
+        raise ValueError("Invalid URL")
+
+
+def scrape(url):
+    print("Starting")
+    video_id = get_video_id_from_url(url)
+    transcript = get_transcript(video_id)
+
+    with open(f'{video_id} TRANSCRIPT', "a") as file:
+        file.write(transcript)
+
+    print("Scraping Done")
 
 
 class KnowledgeScraperUI(ctk.CTk):
@@ -21,15 +61,15 @@ class KnowledgeScraperUI(ctk.CTk):
         # Input Field
         self.input_field = ctk.CTkEntry(master=self.main_frame, font=("Roboto", 20), width=320, corner_radius=10)
         self.input_field.grid(row=4, pady=5, sticky="nswe")
-        self.input_field.bind("<Return>", lambda event: self.fetch_yt_transcript())
+        self.input_field.bind("<Return>", lambda event: scrape(self.input_field.get()))
         # Button
-        self.submit_button = ctk.CTkButton(master=self.main_frame, width=320, font=("Roboto", 20), text="Submit", corner_radius=10, command=self.fetch_yt_transcript)
+        self.submit_button = ctk.CTkButton(master=self.main_frame, width=320, font=("Roboto", 20), text="Submit", corner_radius=10, command=lambda: scrape(self.input_field.get()))
         self.submit_button.grid(row=5, pady=5, sticky="nswe")
 
-    def fetch_yt_transcript(self):
-        user_input = self.input_field.get()
-        print(f"User Input: {user_input}")
 
+# TODO: get video ID from a link
+# TODO: get video title from a link
+# TODO: download the transcript into a .txt file and name it it's title
 
 if __name__ == "__main__":
     app = KnowledgeScraperUI()
