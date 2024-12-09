e342831a248893cd049b167b4fa7c2c9e23eefc1
dybowskyy <igordyb@gmail.com>
2024-12-09 19:11:09 +0100

    Kinda working

--- main.py
+++ main.py
@@ -3,46 +3,12 @@ import youtube_transcript_api
 import os
 from dotenv import load_dotenv
 from googleapiclient.discovery import build
+import openai
+import re
 
 load_dotenv()
 YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
-
-def get_video_id_from_url(url) -> str:
-    if "v=" in url:
-        return url.split("v=")[1].split("&")[0]
-    elif "youtu.be/" in url:
-        return url.split("youtu.be/")[1]
-    else:
-        raise ValueError("Invalid URL")
-
-def get_video_title(video_id) -> str:
-    youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
-    request = youtube.videos().list(part="snippet", id=video_id)
-    response = request.execute()
-
-    if 'items' in response and len(response['items']) > 0:
-        title = response['items'][0]['snippet']['title']
-        return title
-    else:
-        print("Video not found")
-
-def get_transcript(video_id) -> str:
-    try:
-        transcript = youtube_transcript_api.YouTubeTranscriptApi.get_transcript(video_id)
-        return "\n".join([f"{item['text']}" for item in transcript])
-    except youtube_transcript_api.TranscriptsDisabled:
-        return "Transcripts are disabled for this video."
-    except youtube_transcript_api.TooManyRequests:
-        return "Too many requests"
-    except youtube_transcript_api.CouldNotRetrieveTranscript:
-        return "Could not retrieve transcript"
-
-def scrape(url):
-    video_id = get_video_id_from_url(url)
-    transcript = get_transcript(video_id)
-
-    with open(f'{get_video_title(get_video_id_from_url(url))}', "a") as file:
-        file.write(transcript)
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 class KnowledgeScraperUI(ctk.CTk):
     def __init__(self):
@@ -68,6 +34,66 @@ class KnowledgeScraperUI(ctk.CTk):
         # Button
         self.submit_button = ctk.CTkButton(master=self.main_frame, width=320, font=("Roboto", 20), text="Submit", corner_radius=10, command=lambda: scrape(self.input_field.get()))
         self.submit_button.grid(row=5, pady=5, sticky="nswe")
+
+def get_video_id_from_url(url) -> str:
+    if "v=" in url:
+        return url.split("v=")[1].split("&")[0]
+    elif "youtu.be/" in url:
+        return url.split("youtu.be/")[1]
+    else:
+        raise ValueError("Invalid URL")
+
+def get_video_title(video_id) -> str:
+    youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
+    request = youtube.videos().list(part="snippet", id=video_id)
+    response = request.execute()
+
+    if 'items' in response and len(response['items']) > 0:
+        title = response['items'][0]['snippet']['title']
+        return title
+    else:
+        print("Video not found")
+def get_transcript(video_id) -> str:
+    try:
+        transcript = youtube_transcript_api.YouTubeTranscriptApi.get_transcript(video_id)
+        return "\n".join([f"{item['text']}" for item in transcript])
+    except youtube_transcript_api.TranscriptsDisabled:
+        return "Transcripts are disabled for this video."
+    except youtube_transcript_api.TooManyRequests:
+        return "Too many requests"
+    except youtube_transcript_api.CouldNotRetrieveTranscript:
+        return "Could not retrieve transcript"
+
+def scrape(url):
+    video_id = get_video_id_from_url(url)
+    transcript = get_transcript(video_id)
+
+    with open(f'{get_video_title(get_video_id_from_url(url))}', "a") as file:
+        file.write(str(create_note_from_transcript(transcript)))
+
+def create_note_from_transcript(transcript):
+    prompt = open(r'prompt.txt', 'r')
+
+    try:
+        response = openai.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "system", "content": "You make high quality notes"}, {"role": "user", "content": f'{prompt.read()}\n\nTranscript: {transcript}'}]
+        ),
+        prompt.close()
+
+        return response
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        prompt.close()
+        return None
+
+#TODO: Add error handling for invalid URLs, YouTube API rate limits, and OpenAI API errors
+#TODO: Sanitize user input & and video title to prevent vulnerabilities
+#TODO: Display the notes in a UI OR display them in Obsidian
+#TODO: Simplify the code
+#TODO: Improve UI: add a progress bar, message box for errors, etc.
+
 
 if __name__ == "__main__":
     app = KnowledgeScraperUI()
